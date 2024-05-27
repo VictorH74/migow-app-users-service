685ac876dbb97f0db138ad5b685398202ca46a5b
VictorH74 <victorh.almeida7@gmail.com>
2024-05-27 07:23:41 -0300

    build: initial files

---
+++ src/test/java/com/service/users/migow/migow_users_service/MigowUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.users.migow.migow_users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigowUsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
