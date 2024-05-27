dca44d9fe370bc74b200520e7601916d0e32ccd6
VictorH74 <victorh.almeida7@gmail.com>
2024-05-27 07:24:18 -0300

    extra

---
+++ src/main/java/com/service/users/migow/migow_users_service/MigowUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.users.migow.migow_users_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigowUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigowUsersServiceApplication.class, args);
+	}
+
+}
